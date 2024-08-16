82f2a3a5e4e7adcdca51ecaf3203d3b35a532891
Tallyson Freitas <tallyson.eu@outlook.com>
2024-08-16 19:15:09 -0300

    feat: create Logos and ProductsDetails components

--- app/ui/Home.tsx
+++ app/ui/Home.tsx
@@ -10,7 +10,7 @@ export function Header() {
     ]
 
     return (
-        <header className="border-b border-gray-300 fixed w-full top-0">
+        <header className="border-b border-gray-300 fixed w-full top-0 bg-white">
             <div className="container mx-auto p-4">
                 <ul className="flex gap-5">
                     {HeadMenuItems.map((each) => (
@@ -26,10 +26,7 @@ export function Header() {
 
 export function ProductState() {
     return (
-        <div
-            className="container mx-auto py-24 flex flex-col gap-10 items-center"
-            style={{ maxWidth: '800px' }}
-        >
+        <div className="container mx-auto py-24 flex flex-col gap-10 items-center max-w-800">
             <h1 className="text-7xl font-bold pt-10">The Product Name</h1>
             <Image
                 className="rounded-xl"
@@ -49,6 +46,65 @@ export function ProductState() {
             >
                 Buy Now
             </a>
+        </div>
+    )
+}
+
+export function LogosFront() {
+    const Logos = ['Logo', 'Logo', 'Logo', 'Logo']
+
+    return (
+        <div className="container mx-auto flex justify-center items-center gap-10 pb-24">
+            {Logos.map((each) => (
+                <p className="text-4xl text-gray-999 font-bold" key={each}>
+                    {each}
+                </p>
+            ))}
+        </div>
+    )
+}
+
+export function ProductsDetails() {
+    return (
+        <div className="container mx-auto p-24 grid grid-cols-2 gap-8">
+            <Image
+                src={'https://placehold.co/500x356/png'}
+                className="w-full rounded-xl"
+                width={500}
+                height={356}
+                alt="imagem"
+            />
+            <div className="flex flex-col gap-5 justify-center items-start">
+                <h2 className="font-bold text-4xl">Fast</h2>
+                <p className="font-medium text-xl text-gray-888">
+                    You&apos;ve never made a website this fast before.
+                </p>
+                <a
+                    className="bg-gray-800 text-white text-sm font-semibold px-4 py-2 rounded-lg shadow-2xl"
+                    href="#"
+                >
+                    Read More
+                </a>
+            </div>
+            <div className="flex flex-col gap-5 justify-center items-start">
+                <h2 className="font-bold text-4xl">Easy</h2>
+                <p className="font-medium text-xl text-gray-888">
+                    Works like the canvas tools you&apos;re familiar with.
+                </p>
+                <a
+                    className="bg-gray-800 text-white text-sm font-semibold px-4 py-2 rounded-lg shadow-2xl"
+                    href="#"
+                >
+                    Read More
+                </a>
+            </div>
+            <Image
+                src={'https://placehold.co/500x356/png'}
+                className="w-full rounded-xl"
+                width={500}
+                height={356}
+                alt="imagem"
+            />
         </div>
     )
 }
